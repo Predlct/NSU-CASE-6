dffbd6a3354cda6918f935852a6d42d954311c6e
kost960 <pozitq3@gmail.com>
2024-09-15 15:23:01 +0700

    fix count syllables

--- readability_index.py
+++ readability_index.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from langdetect import detect
+import syllables
 
 text = input('Введите текст: ')
 text = text.replace('!!!', '!')
@@ -7,23 +8,18 @@ text = text.replace('???', '?')
 blob = TextBlob(text)
 
 ru_vowel_letters = 'аоуэыяёеюи'
-en_vowel_letters = 'aeiouy'
+en_vowel_letters = 'aeiouyAEIOUY'
 
 print(f'Кол-во предложений: {len(blob.sentences)}')
 print(f'Кол-во слов: {len(blob.words)}')
 
+syllables_count = 0
 if detect(text) != 'en':
-    print(f'Кол-во слогов: {len([x for x in text.lower() if x in ru_vowel_letters])}')
+    syllables_count = len([x for x in text.lower() if x in ru_vowel_letters])
+    print(f'Кол-во слогов: {syllables_count}')
 else:
-    count = 0
-    for word in text.split():
-        if word[0] in en_vowel_letters:
-            count += 1
-        for index in range(1, len(word)):
-            if word[index] in en_vowel_letters and word[index - 1] not in en_vowel_letters:
-                count += 1
-        if word.endswith('e'):
-            count -= 1
-        if count == 0:
-            count += 1
-    print(f'Кол-во слогов: {count}')
+    syllables_count = syllables.estimate(text.lower())
+    print(f'Кол-во слогов: {syllables_count}')
+
+print(f'Средняя длина предложения в словах: {len(blob.words) / len(blob.sentences)}')
+print(f'Средняя длина слова в слогах: {syllables_count / len(blob.words)}')
