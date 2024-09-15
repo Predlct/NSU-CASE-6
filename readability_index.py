fe586d691769c17b23b1ef89b423de93149864f1
kost960 <pozitq3@gmail.com>
2024-09-15 10:47:05 +0700

    sentences, letters, syllables count

--- readability_index.py
+++ readability_index.py
@@ -1,15 +1,29 @@
 from textblob import TextBlob
-from ruts import BasicStats
 from langdetect import detect
 
-text = "War doesn't show who's right, just who's left."
+text = input('Введите текст: ')
 text = text.replace('!!!', '!')
 text = text.replace('???', '?')
 blob = TextBlob(text)
+
+ru_vowel_letters = 'аоуэыяёеюи'
+en_vowel_letters = 'aeiouy'
 
 print(f'Кол-во предложений: {len(blob.sentences)}')
 print(f'Кол-во слов: {len(blob.words)}')
 
 if detect(text) != 'en':
-    bs = BasicStats(text)
-    print(f'Кол-во слогов: {bs.get_stats()['n_syllables']}')
+    print(f'Кол-во слогов: {len([x for x in text.lower() if x in ru_vowel_letters])}')
+else:
+    count = 0
+    for word in text.split():
+        if word[0] in en_vowel_letters:
+            count += 1
+        for index in range(1, len(word)):
+            if word[index] in en_vowel_letters and word[index - 1] not in en_vowel_letters:
+                count += 1
+        if word.endswith('e'):
+            count -= 1
+        if count == 0:
+            count += 1
+    print(f'Кол-во слогов: {count}')
