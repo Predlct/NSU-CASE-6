c9ffed85c38aa44ea59162fc1306efde4bbdb991
predlct <p.redict@mail.ru>
2024-09-16 21:19:40 +0700

    local

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from ru_readability_index import get_russian_text_stats
 from en_readability_index import get_english_text_stats
 
 language_choose = input('Select text languages (ru/en): ')
-text = input('Введите текст: ')
+text = input('Enter text: ')
 
 if language_choose == 'ru':
     get_russian_text_stats(text)
 elif language_choose == 'en':
     get_english_text_stats(text)
 else:
-    print('Язык введён неверно!')
+    print('Language is incorrect!')
